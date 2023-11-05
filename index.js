c206cee5ef0ce43a8844c1713fa49a8f408a7c50
Debdipto Biswas <127871393+DebdiptoBiswas@users.noreply.github.com>
2023-11-05 23:28:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ document.querySelector("img.img1").setAttribute("src",randomDiceImg1);
 document.querySelector("img.img2").setAttribute("src",randomDiceImg2);
 if(randomnumber1 === randomnumber2){document.querySelector("h1").innerHTML = "Draw";}
 else if(randomnumber1 < randomnumber2){document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";}
-else if(randomnumber1 > randomnumber2){document.querySelector("h1").innerHTML = "🚩	Player 1 Wins!";}
+else if(randomnumber1 > randomnumber2){document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";}
